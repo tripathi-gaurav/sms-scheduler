432572ad698639875dac083a999e0c049e660964
Saurabh Bothra <saurabhbothra29@gmail.com>
2019-11-30 22:22:45 -0500

    improved layout of message display and overall flow of app

--- lib/sms_scheduler_web/controllers/page_controller.ex
+++ lib/sms_scheduler_web/controllers/page_controller.ex
@@ -3,8 +3,14 @@ defmodule SmsSchedulerWeb.PageController do
   alias SmsScheduler.Users
 
   def new(conn, _params) do
+    login_user = get_session(conn, :user)
     user = Users.new_user()
-    render(conn, "new.html", user: user)
+    if(login_user == nil) do
+        render(conn, "new.html", user: user, current_user: nil)
+    else
+      current_user = Users.get_user(login_user.id)
+      render(conn, "new.html", user: user, current_user: current_user)
+    end
   end
 
   def index(conn, _params) do
